5280fe8a1453e95262b58b022b57181f7e4da7d7
George Field <georgefield@Georges-MacBook-Pro-2.local>
2022-10-14 13:27:22 +0100

    initial commit

---
+++ features/automation/optimization/autoTakeProfit/tests/autoTakeProfitFormChange.test.ts
@@ -0,0 +1,22 @@
+import { expect } from 'chai';
+
+test('Test autotake profit form component', () => {
+
+    describe('User inputs values into form', () => {
+
+        it('Should validate the input for  x', () => {
+
+        })
+
+        it('Should disable button if values are incorrect', () => {
+
+        })
+
+
+        it('Should enable button if values are correct', () => {
+
+        })
+    })
+
+
+})
